cc9175f2e96d53d6acc4d464c2993245a6f67b91
ajeeraba <ajeeraba3869837@users.noreply.github.com>
2023-03-11 09:00:32 -0500

    initial commit

---
+++ PyBank/main.py
@@ -0,0 +1,31 @@
+# Import Dependencies
+import pandas as pd
+
+# File to Load
+bank_path = "Resources/budget_data.csv"
+
+# Read the modified Comic Books csv and store into Pandas DataFrame
+bank_df = pd.read_csv(bank_path, encoding='utf-8')
+
+#rename the column Profit/Loss
+bank_df.rename(columns={'Profit/Losses':'profit'}, inplace=True)
+
+#calculate net profit or loss
+net_profit = bank_df['profit'].sum()
+
+#Count the number of months
+net_profit_loss = bank_df['profit'].sum()
+no_months = bank_df.shape[0]
+
+#create a column in the dataframe to store the calculated change
+bank_df['change'] = bank_df['profit'].diff(periods=1)
+
+#Calculate the average profit/loss, the lagest and smallestchange 
+average = round(bank_df['change'].mean(), 2)
+largest = int(bank_df['change'].max())
+smallest = int(bank_df['change'].min())
+
+#print the output as required
+print(f'Total Months: {no_months} \n Total: ${net_profit} \n Average Change: ${average} \n Greatest Increase in Profits: (${largest}) \n Greatest Decrease in Profits: (${smallest})')
+
+
---
+++ PyPoll/main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+# File to Load
+poll_path = "Resources/election_data.csv"
+
+# Read the modified Comic Books csv and store into Pandas DataFrame
+poll_df = pd.read_csv(poll_path)
+
+# total votes cast
+votes = poll_df.shape[0]
+
+#List of Candidates who got votes
+candidate_name = poll_df['Candidate'].unique()
+
+#votes garnered
+votes_received = poll_df.groupby(['Candidate'])['Ballot ID'].count()
+
+#percent votes garnered
+percent_votes = round(votes_received/len(poll_df)*100, 2)
+
+#election winner
+winner = percent_votes.max()
+
+#results tally
+print(f' Election Results \n -------------------- \n Total Votes: {votes} \n -------------------- \n {percent_votes} ({votes_received}) \n  -------------------- \n Winner: {candidate_name[1]} \n ------------------------')
