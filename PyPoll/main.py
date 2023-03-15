c245889036baae1b5bb8bdb9b7e47483393a460f
ajeeraba <ajeeraba3869837@users.noreply.github.com>
2023-03-14 20:02:49 -0400

    printed empty text file updating it

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -18,12 +18,13 @@ votes_received = poll_df.groupby(['Candidate'])['Ballot ID'].count()
 #percent votes garnered
 percent_votes = round(votes_received/len(poll_df)*100, 2)
 
+
 #election winner
 winner = percent_votes.max()
 
 #results tally
-print(f' Election Results \n -------------------- \n Total Votes: {votes} \n -------------------- \n {percent_votes} ({votes_received}) \n  -------------------- \n Winner: {candidate_name[1]} \n ------------------------')
+print(f' Election Results \n -------------------- \n Total Votes: {votes} \n -------------------- \n{percent_votes} ({votes_received})  \n  -------------------- \n Winner: {candidate_name[1]} \n ------------------------')
 
 #print results to txt file
 with open('poll.txt', 'a') as f:
-    print(f' Election Results \n -------------------- \n Total Votes: {votes} \n -------------------- \n {percent_votes} ({votes_received}) \n  -------------------- \n Winner: {candidate_name[1]} \n ------------------------')
+    print(f' Election Results \n -------------------- \n Total Votes: {votes} \n -------------------- \n {percent_votes} ({votes_received})  \n  -------------------- \n Winner: {candidate_name[1]} \n ------------------------')
